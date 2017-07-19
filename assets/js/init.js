cd4dce8eac513cef6cafa224d5251db3a6b7ee5a
tmontanaro <teodoro.montanaro@polito.it>
2017-07-19 17:20:32 +0200

    minor changes to remove the menu button from mobile version

--- assets/js/init.js
+++ assets/js/init.js
@@ -37,7 +37,7 @@
 					side: 'top',
 					height: 44,
 					width: '100%',
-					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>'
+					html: '<span class="title" data-action="copyHTML" data-args="logo"></span>'
 				}
 			}
 		}
